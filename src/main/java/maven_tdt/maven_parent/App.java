a5f1e9747750b6eb101fc8d2e9a3d099548583c7
Thanh phuong <phuongphi11111@gmail.com>
2019-03-02 23:45:35 +0700

    add maven

---
+++ src/main/java/maven_tdt/maven_parent/App.java
@@ -0,0 +1,13 @@
+package maven_tdt.maven_parent;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
